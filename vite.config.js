8e364cac7a887a4c89b6a42291640cd160e6c72a
KratikaPrabhu <prabhukrati62@gmail.com>
2025-09-16 23:27:17 +0530

    Made some changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
+  base: '/',  // Important for Vercel deployment
   plugins: [react()],
   resolve: {
     alias: {
